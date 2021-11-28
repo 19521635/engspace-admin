19f0676627e51ed9baa6b2f6b90907c7ac3601d2
19521635 <19521635@gm.uit.edu.vn>
2021-11-29 00:49:34 +0700

    👽️ sửa lại api base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-	// baseURL: "https://engspace-be-ff5wy.ondigitalocean.app/api",
-	baseURL: "http://localhost:8000/api",
+	baseURL: "https://engspace-be-ff5wy.ondigitalocean.app/api",
+	// baseURL: "http://localhost:8000/api",
 	headers: {
 		"Content-Type": "application/json",
 	},
